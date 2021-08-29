d4f089355802349aa376f02803b2131418f1f987
niles <niles@LAPTOP-3K24CQ1R>
2021-08-29 21:55:00 +0530

    Add project files.

---
+++ ConsoleApp_Unary_Operator/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_Unary_Operator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 15;
+            int result;
+
+            Console.WriteLine("a is " + a);
+            result = ++a;
+            Console.WriteLine("Pre increment of a is : " + a);
+
+            Console.WriteLine("a is " + a);
+            result = a++;
+            Console.WriteLine("Post increment of a is : " + a);
+
+            Console.WriteLine("a is " + a);
+            result = --a;
+            Console.WriteLine("Pre decrement of a is : " + a);
+
+            Console.WriteLine("a is " + a);
+            result = a--;
+            Console.WriteLine("Post decrement of a is : " + a);
+
+            Console.ReadLine();
+
+        }
+    }
+}
